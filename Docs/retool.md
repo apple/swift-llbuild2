90e2fe0016370773e9471eff17ba4d7fe13b8878
Ankit Aggarwal <ankit_aggarwal@apple.com>
2020-05-28 11:54:53 -0700

    [RETool] Add a command to dump server capabilities

--- Docs/retool.md
+++ Docs/retool.md
@@ -2,4 +2,10 @@
 
 `retool` is a low-level helper tool for interacting with the remote execution services.
 
-More documentation coming soon...
+## capabilities
+
+Dump server capabilities. This command is also useful for testing connection with the remote server.
+
+```sh
+$ retool capabilities --url grpc://localhost:8980 --instance-name remote-execution
+```
--- Sources/Tools/retool/Capabilities.swift
+++ Sources/Tools/retool/Capabilities.swift
@@ -6,21 +6,28 @@
 // See http://swift.org/LICENSE.txt for license information
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
+import TSCBasic
 import Foundation
 import ArgumentParser
 
+import GRPC
 import LLBRETool
 
 struct Capabilities: ParsableCommand {
 
-    @Option()
-    var frontendURL: URL
+    @Option(help: "The gRPC endpoint of the Bazel RE2 server")
+    var url: URL
+
+    @Option(help: "The instance of the execution system to operate against")
+    var instanceName: String?
 
     func run() throws {
-        let options: LLBRETool.Options = .init(
-            frontendURL: frontendURL
+        let options = try LLBRETool.Options(
+            frontend: url.toConnectionTarget()
         )
+
         let tool = RETool(options)
-        tool.getCapabilities()
+        let response = try tool.getCapabilities(instanceName: instanceName).wait()
+        print(response)
     }
 }
