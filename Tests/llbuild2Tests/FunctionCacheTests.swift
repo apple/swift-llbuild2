d1871cd6d1a59a52d630c33931348db61d2dc295
David M. Bryson <dmbryson@apple.com>
2020-06-22 14:28:05 -0700

    Add file-backed function cache implementation

---
+++ Tests/llbuild2Tests/FunctionCacheTests.swift
@@ -0,0 +1,42 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+import XCTest
+
+import llbuild2
+import TSCBasic
+
+final class FunctionCacheTests: XCTestCase {
+    let group = LLBMakeDefaultDispatchGroup()
+
+    /// ${TMPDIR} or just "/tmp", expressed as AbsolutePath
+    private var temporaryPath: AbsolutePath {
+        return AbsolutePath(ProcessInfo.processInfo.environment["TMPDIR", default: "/tmp"])
+    }
+
+    func doFunctionCacheTests(cache: LLBFunctionCache) throws {
+        XCTAssertNil(try cache.get(key: "key1").wait())
+
+        let id1 = LLBDataID(blake3hash: LLBByteBuffer.withBytes(ArraySlice("value1".utf8)), refs: [])
+        try cache.update(key: "key1", value: id1).wait()
+        XCTAssertEqual(id1, try cache.get(key: "key1").wait())
+    }
+
+    func testInMemoryFunctionCache() throws {
+        let cache = LLBInMemoryFunctionCache(group: group)
+        try doFunctionCacheTests(cache: cache)
+    }
+
+    func testFileBackedFunctionCache() throws {
+        try withTemporaryDirectory(dir: temporaryPath, prefix: "LLBFunctionCacheTests" + #function, removeTreeOnDeinit: true) { tmpDir in
+            let cache = LLBFileBackedFunctionCache(group: group, path: tmpDir)
+            try doFunctionCacheTests(cache: cache)
+        }
+
+    }
+}
