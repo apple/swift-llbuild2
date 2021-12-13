581e2034828deac4989864b7346552b561d8dfd3
Randy Becker <81446220+randy-becker@users.noreply.github.com>
2021-12-13 15:57:13 -0500

    Add process execution action (#133)

---
+++ Sources/llbuild2fx/SpawnProcess.swift
@@ -0,0 +1,345 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+import Foundation
+import TSCBasic
+import TSCUtility
+import TSFCAS
+import TSFCASFileTree
+import TSFFutures
+
+extension Foundation.Process {
+    private enum ProcessTerminationError: Error {
+        case nonZeroExit(Int32)
+        case signaled(Int32)
+        case unknown(Int32)
+    }
+
+    private struct ProcessKiller: LLBCancelProtocol {
+        private let runningProcess: Foundation.Process
+        init(runningProcess: Foundation.Process) {
+            self.runningProcess = runningProcess
+        }
+
+        func cancel(reason: String?) {
+            let pid = runningProcess.processIdentifier
+            kill(pid, SIGKILL)
+        }
+    }
+
+    fileprivate func runLater(_ ctx: Context) -> LLBCancellableFuture<Void> {
+        let completionPromise: LLBPromise<Void> = ctx.group.next().makePromise()
+
+        self.terminationHandler = { process in
+            let status = process.terminationStatus
+
+            switch process.terminationReason {
+            case .exit:
+                switch status {
+                case 0:
+                    completionPromise.succeed(())
+                default:
+                    completionPromise.fail(ProcessTerminationError.nonZeroExit(status))
+                }
+            case .uncaughtSignal:
+                completionPromise.fail(ProcessTerminationError.signaled(status))
+            @unknown default:
+                completionPromise.fail(ProcessTerminationError.unknown(status))
+            }
+        }
+
+        do {
+            try run()
+        } catch {
+            completionPromise.fail(error)
+        }
+
+        let canceller = LLBCanceller(ProcessKiller(runningProcess: self))
+
+        return LLBCancellableFuture(completionPromise.futureResult, canceller: canceller)
+    }
+}
+
+public struct ProcessSpec: Codable {
+    public enum Executable: Codable {
+        case absolutePath(AbsolutePath)
+        case inputPath(RelativePath)
+
+        #if swift(<5.5)
+        enum CodingKeys: CodingKey {
+            case absolutePath
+            case inputPath
+        }
+
+        enum AbsolutePathCodingKeys: CodingKey {
+            case _0
+        }
+
+        enum InputPathCodingKeys: CodingKey {
+            case _0
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case let .absolutePath(value):
+                var nestedContainer = container.nestedContainer(
+                    keyedBy: AbsolutePathCodingKeys.self, forKey: .absolutePath)
+                try nestedContainer.encode(value, forKey: ._0)
+            case let .inputPath(value):
+                var nestedContainer = container.nestedContainer(keyedBy: InputPathCodingKeys.self, forKey: .inputPath)
+                try nestedContainer.encode(value, forKey: ._0)
+            }
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            if container.allKeys.count != 1 {
+                let context = DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Invalid number of keys found, expected one.")
+                throw DecodingError.typeMismatch(Executable.self, context)
+            }
+
+            switch container.allKeys.first.unsafelyUnwrapped {
+            case .absolutePath:
+                let nestedContainer = try container.nestedContainer(
+                    keyedBy: AbsolutePathCodingKeys.self, forKey: .absolutePath)
+                self = .absolutePath(try nestedContainer.decode(AbsolutePath.self, forKey: ._0))
+            case .inputPath:
+                let nestedContainer = try container.nestedContainer(
+                    keyedBy: InputPathCodingKeys.self, forKey: .inputPath)
+                self = .inputPath(try nestedContainer.decode(RelativePath.self, forKey: ._0))
+            }
+        }
+        #endif
+    }
+
+    public enum RuntimeValue: Codable {
+        case literal(String)
+        case inputPath(RelativePath)
+        case outputPath(RelativePath)
+
+        #if swift(<5.5)
+        enum CodingKeys: CodingKey {
+            case literal
+            case inputPath
+            case outputPath
+        }
+
+        enum LiteralCodingKeys: CodingKey {
+            case _0
+        }
+
+        enum InputPathCodingKeys: CodingKey {
+            case _0
+        }
+
+        enum OutputPathCodingKeys: CodingKey {
+            case _0
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case let .literal(value):
+                var nestedContainer = container.nestedContainer(keyedBy: LiteralCodingKeys.self, forKey: .literal)
+                try nestedContainer.encode(value, forKey: ._0)
+            case let .inputPath(value):
+                var nestedContainer = container.nestedContainer(keyedBy: InputPathCodingKeys.self, forKey: .inputPath)
+                try nestedContainer.encode(value, forKey: ._0)
+            case let .outputPath(value):
+                var nestedContainer = container.nestedContainer(keyedBy: OutputPathCodingKeys.self, forKey: .outputPath)
+                try nestedContainer.encode(value, forKey: ._0)
+            }
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            if container.allKeys.count != 1 {
+                let context = DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Invalid number of keys found, expected one.")
+                throw DecodingError.typeMismatch(RuntimeValue.self, context)
+            }
+
+            switch container.allKeys.first.unsafelyUnwrapped {
+            case .literal:
+                let nestedContainer = try container.nestedContainer(keyedBy: LiteralCodingKeys.self, forKey: .literal)
+                self = .literal(try nestedContainer.decode(String.self, forKey: ._0))
+            case .inputPath:
+                let nestedContainer = try container.nestedContainer(
+                    keyedBy: InputPathCodingKeys.self, forKey: .inputPath)
+                self = .inputPath(try nestedContainer.decode(RelativePath.self, forKey: ._0))
+            case .outputPath:
+                let nestedContainer = try container.nestedContainer(
+                    keyedBy: OutputPathCodingKeys.self, forKey: .outputPath)
+                self = .outputPath(try nestedContainer.decode(RelativePath.self, forKey: ._0))
+            }
+        }
+        #endif
+    }
+
+    let executable: Executable
+    let arguments: [RuntimeValue]
+    let environment: [String: RuntimeValue]
+
+    let stdinSource: RelativePath?
+    let stdoutDestination: RelativePath?
+    let stderrDestination: RelativePath?
+
+    public init(
+        executable: Executable,
+        arguments: [RuntimeValue] = [],
+        environment: [String: RuntimeValue] = [:],
+        stdinSource: RelativePath? = nil,
+        stdoutDestination: RelativePath? = nil,
+        stderrDestination: RelativePath? = nil
+    ) {
+        self.executable = executable
+        self.arguments = arguments
+        self.environment = environment
+        self.stdinSource = stdinSource
+        self.stdoutDestination = stdoutDestination
+        self.stderrDestination = stderrDestination
+    }
+
+    fileprivate func process(inputPath: AbsolutePath, outputPath: AbsolutePath) -> Foundation.Process {
+        let runtimeValueMapper: (RuntimeValue) -> String = { value in
+            switch value {
+            case .literal(let v):
+                return v
+            case .inputPath(let path):
+                return inputPath.appending(path).pathString
+            case .outputPath(let path):
+                return outputPath.appending(path).pathString
+            }
+        }
+
+        let exePath: AbsolutePath
+        switch executable {
+        case .absolutePath(let path):
+            exePath = path
+        case .inputPath(let path):
+            exePath = inputPath.appending(path)
+        }
+
+        let process = Process()
+
+        process.executableURL = URL(fileURLWithPath: exePath.pathString)
+        process.arguments = arguments.map(runtimeValueMapper)
+        process.environment = environment.mapValues(runtimeValueMapper)
+
+        let devNull = "/dev/null"
+
+        if let stdin = stdinSource {
+            process.standardInput = FileHandle(forReadingAtPath: inputPath.appending(stdin).pathString)
+        } else {
+            process.standardInput = FileHandle(forReadingAtPath: devNull)
+        }
+
+        if let stdout = stdoutDestination {
+            process.standardOutput = FileHandle(forWritingAtPath: outputPath.appending(stdout).pathString)
+        } else {
+            process.standardOutput = FileHandle(forWritingAtPath: devNull)
+        }
+
+        if let stderr = stderrDestination {
+            process.standardError = FileHandle(forWritingAtPath: outputPath.appending(stderr).pathString)
+        } else {
+            process.standardError = FileHandle(forWritingAtPath: devNull)
+        }
+
+        return process
+    }
+}
+
+struct ProcessInputTree: FXTreeID {
+    let dataID: LLBDataID
+}
+
+extension SpawnProcess: FXAction {
+    private enum SpawnProcessError: Swift.Error {
+        case emptyRefs
+        case tooManyRefs
+    }
+
+    public var refs: [LLBDataID] { [inputTree.dataID] }
+    public var codableValue: ProcessSpec { spec }
+
+    public init(refs: [LLBDataID], codableValue: ProcessSpec) throws {
+        guard !refs.isEmpty else {
+            throw SpawnProcessError.emptyRefs
+        }
+        guard refs.count == 1 else {
+            throw SpawnProcessError.tooManyRefs
+        }
+
+        let treeID = refs[0]
+
+        self.init(inputTree: ProcessInputTreeID(dataID: treeID), spec: codableValue)
+    }
+}
+
+public struct SpawnProcess {
+    let inputTree: ProcessInputTreeID
+
+    let spec: ProcessSpec
+
+    public init(
+        inputTree: ProcessInputTreeID,
+        spec: ProcessSpec
+    ) {
+        self.inputTree = inputTree
+        self.spec = spec
+    }
+
+    private enum FXSpawnError: Error {
+        case failure(outputTree: LLBDataID, underlyingError: Error)
+        case recoveryUploadFailure(uploadError: Error, originalError: Error)
+    }
+
+    public func run(_ ctx: Context) -> LLBFuture<ProcessOutputTreeID> {
+        inputTree.materialize(ctx) { inputPath in
+            withTemporaryDirectory(ctx) { outputPath in
+                let process = spec.process(inputPath: inputPath, outputPath: outputPath)
+                let cancellable = process.runLater(ctx)
+
+                ctx.fxApplyDeadline(cancellable)
+
+                return cancellable.future.flatMap { _ in
+                    LLBCASFileTree.import(path: outputPath, to: ctx.db, ctx).map { treeID in
+                        ProcessOutputTreeID(dataID: treeID)
+                    }
+                }.flatMapError { error in
+                    return LLBCASFileTree.import(path: outputPath, to: ctx.db, ctx).flatMapThrowing { treeID in
+                        throw FXSpawnError.failure(outputTree: treeID, underlyingError: error)
+                    }.flatMapErrorThrowing { uploadError in
+                        throw FXSpawnError.recoveryUploadFailure(uploadError: uploadError, originalError: error)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension SpawnProcess: Encodable {}
+
+public struct ProcessInputTreeID: FXSingleDataIDValue, FXTreeID {
+    public let dataID: LLBDataID
+    public init(dataID: LLBDataID) {
+        self.dataID = dataID
+    }
+}
+
+public struct ProcessOutputTreeID: FXSingleDataIDValue, FXTreeID {
+    public let dataID: LLBDataID
+    public init(dataID: LLBDataID) {
+        self.dataID = dataID
+    }
+}
---
+++ Sources/llbuild2fx/TreeMaterialization.swift
@@ -0,0 +1,53 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+import TSCBasic
+import TSCUtility
+
+public protocol FXTreeMaterializer {
+    func materialize(tree: FXTreeID) -> AbsolutePath?
+}
+
+private class ContextTreeMaterializer {}
+
+extension Context {
+    public var fxTreeMaterializer: FXTreeMaterializer? {
+        get {
+            guard let value = self[ObjectIdentifier(ContextTreeMaterializer.self)] as? FXTreeMaterializer else {
+                return nil
+            }
+
+            return value
+        }
+        set {
+            self[ObjectIdentifier(ContextTreeMaterializer.self)] = newValue
+        }
+    }
+}
+
+func withTemporaryDirectory<R>(_ ctx: Context, _ body: (AbsolutePath) -> LLBFuture<R>) -> LLBFuture<R> {
+    do {
+        return try withTemporaryDirectory(removeTreeOnDeinit: true, body)
+    } catch {
+        return ctx.group.next().makeFailedFuture(error)
+    }
+}
+
+extension FXTreeID {
+    func materialize<R>(_ ctx: Context, _ body: @escaping (AbsolutePath) -> LLBFuture<R>) -> LLBFuture<R> {
+        if let path = ctx.fxTreeMaterializer?.materialize(tree: self) {
+            return body(path)
+        }
+
+        return withTemporaryDirectory(ctx) { tmp in
+            LLBCASFileTree.export(self.dataID, from: ctx.db, to: tmp, ctx).flatMap {
+                body(tmp)
+            }
+        }
+    }
+}
