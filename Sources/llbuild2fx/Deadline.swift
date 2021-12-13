7eb42357f24339415f1a4d7cee6e289af51c7118
Randy Becker <81446220+randy-becker@users.noreply.github.com>
2021-12-13 15:53:25 -0500

    Add process execution action

---
+++ Sources/llbuild2fx/Deadline.swift
@@ -0,0 +1,51 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+import Foundation
+import NIO
+import TSCUtility
+import TSFFutures
+
+private class DeadlineKey {}
+
+extension Context {
+    public var fxDeadline: Date? {
+        get {
+            self[ObjectIdentifier(DeadlineKey.self)] as? Date
+        }
+        set {
+            self[ObjectIdentifier(DeadlineKey.self)] = newValue
+        }
+    }
+
+    public var nioDeadline: NIODeadline? {
+        guard let foundationDeadline = fxDeadline else {
+            return nil
+        }
+
+        let then = NIODeadline.now()
+        let now = Date()
+        let timeLeft: TimeInterval = foundationDeadline.timeIntervalSince(now)
+        let milliseconds = timeLeft * 1000
+        let microseconds = milliseconds * 1000
+        let nanoseconds = Int64(microseconds * 1000)
+        return then + TimeAmount.nanoseconds(nanoseconds)
+    }
+
+    public func fxApplyDeadline<T>(_ cancellable: LLBCancellableFuture<T>) {
+        if let actualDeadline = nioDeadline {
+            let timer = cancellable.future.eventLoop.scheduleTask(deadline: actualDeadline) {
+                cancellable.cancel(reason: "timeout")
+            }
+
+            cancellable.future.whenComplete {
+                _ in timer.cancel()
+            }
+        }
+    }
+}
