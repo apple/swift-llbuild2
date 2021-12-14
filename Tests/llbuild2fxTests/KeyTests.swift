bcc6f4ae97aa5dbf5345e1b857f061fe21203b3f
Randy Becker <81446220+randy-becker@users.noreply.github.com>
2021-12-14 11:59:56 -0500

    Add tests for FXSingleDataIDValue key encoding

---
+++ Tests/llbuild2fxTests/KeyTests.swift
@@ -0,0 +1,49 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+import llbuild2fx
+import XCTest
+
+final class FXKeyTests: XCTestCase {
+    private struct TestValue: FXSingleDataIDValue {
+        let dataID: LLBDataID
+    }
+
+    func testDataIDKeyEncoding() throws {
+        struct TestKey: FXKey {
+            let dataID: LLBDataID
+            func computeValue(_ fi: FXFunctionInterface<Self>, _ ctx: Context) -> LLBFuture<TestValue> {
+                ctx.group.next().makeSucceededFuture(TestValue(dataID))
+            }
+        }
+
+        let sensitive = "0~AA=="
+        let key = TestKey(dataID: LLBDataID(string: sensitive)!)
+
+        let encoded = try CommandLineArgsEncoder().encode(key)
+
+        XCTAssertEqual(encoded, ["--dataID=\(sensitive)"])
+    }
+
+    func testWrappedKeyEncoding() throws {
+        struct TestKey: FXKey {
+            let value: TestValue
+            func computeValue(_ fi: FXFunctionInterface<Self>, _ ctx: Context) -> LLBFuture<TestValue> {
+                ctx.group.next().makeSucceededFuture(value)
+            }
+        }
+
+        let sensitive = "0~AA=="
+        let key = TestKey(value: TestValue(LLBDataID(string: sensitive)!))
+
+        let encoded = try CommandLineArgsEncoder().encode(key)
+
+        XCTAssertNotEqual(encoded, ["--value=\(sensitive)"])
+        XCTAssertEqual(encoded, ["--value=GtSPSWJwedgG"])
+    }
+}
