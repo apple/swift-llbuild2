a6e0ff28fa5b3dd84417b044fc3186ba3ffca274
David M. Bryson <dmbryson@apple.com>
2025-05-19 17:01:29 -0700

    Add metrics protocol

--- Sources/llbuild2fx/Support/Logging.swift
+++ Sources/llbuild2fx/Support/Logging.swift
@@ -8,17 +8,48 @@
 
 import Logging
 
-/// Support storing and retrieving a logger instance from a Context.
+// Support protocol for recording events during FXKey evaluations
+public protocol FXMetricsSink {
+    func event(
+        _ message: Logger.Message,
+        metadata: @autoclosure () -> Logger.Metadata,
+        _ ctx: Context,
+        file: String,
+        function: String,
+        line: UInt
+    )
+}
+
+extension FXMetricsSink {
+    public func event(
+        _ message: Logger.Message,
+        metadata: @autoclosure () -> Logger.Metadata = [:],
+        _ ctx: Context = .init(),
+        file_ file: String = #file,
+        function: String = #function,
+        line: UInt = #line
+    ) {
+        self.event(message, metadata: metadata(), ctx, file: file, function: function, line: line)
+    }
+}
+
+// Support storing and retrieving logger and metrics instances from a Context.
 public extension Context {
     var logger: Logger? {
         get {
-            guard let logger = self[ObjectIdentifier(Logger.self), as: Logger.self] else {
-                return nil
-            }
-            return logger
+            return self[ObjectIdentifier(Logger.self), as: Logger.self]
         }
         set {
             self[ObjectIdentifier(Logger.self)] = newValue
+        }
+    }
+
+    var metrics: FXMetricsSink? {
+        get {
+            return self[ObjectIdentifier(FXMetricsSink.self), as: FXMetricsSink.self]
+        }
+        set {
+            self[ObjectIdentifier(FXMetricsSink.self)] = newValue
         }
     }
 }
